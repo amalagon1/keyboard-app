15bdf6e969b2ccb7bbb0755edb9acfdae5e7ee97
amalagon1 <89949832+amalagon1@users.noreply.github.com>
2023-01-10 23:40:21 -0500

    got all notes working

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ const play = document.querySelector('.play');
 const regular = document.querySelectorAll('.white-key');
 const sharps = document.querySelectorAll('.black-key');
 const body = document.querySelector('.body');
+const keys = document.querySelectorAll('.key');
+
+
+const note = new Audio("tones/C-sharp.mp3");
+
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    console.log(key.dataset.note)
+    console.log(noteAudio)
+    noteAudio.play()
+}
 
 
 
 
 document.querySelector('.button').addEventListener('click', () => {
+    note.play()
     console.log("clicked")
     body.classList.add('theme-light')
 })
 
 
 
-const playMusic = (newUrl) => {
-    console.log('clicked')
-    let sound = new Audio(newUrl);
-    sound.play()
-}
+// const playMusic = (newUrl) => {
+//     console.log('clicked')
+//     let sound = new Audio(newUrl);
+//     sound.play()
+// }
 
 
 
-// play.addEventListener('click', music)
 
 
-regular.forEach((key, i) => {
-    console.log(key.dataset)
-    const number = i < 2 ? '0' + (i + 8) : (i + 8);
-    const newUrl = "./tones/key" + number + ".mp3"
-    key.addEventListener('click', () => playMusic(newUrl))
-})
+
+// regular.forEach((key, i) => {
+//     console.log(key.dataset)
+//     const number = i < 2 ? '0' + (i + 8) : (i + 8);
+//     const newUrl = "./tones/key" + number + ".mp3"
+//     key.addEventListener('click', () => playMusic(newUrl))
+// })
