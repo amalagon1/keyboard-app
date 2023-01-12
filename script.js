bcb6b966bdb05f01f9e4d83dc4550e7b5af20af2
amalagon1 <89949832+amalagon1@users.noreply.github.com>
2023-01-11 19:33:58 -0500

    linked notes to keys

--- script.js
+++ script.js
@@ -4,16 +4,58 @@ const sharps = document.querySelectorAll('.black-key');
 const body = document.querySelector('.body');
 const keys = document.querySelectorAll('.key');
 
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u'];
 
-const note = new Audio("tones/C-sharp.mp3");
+
+// const note = new Audio("tones/C-sharp.mp3");
 
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
+});
+
+document.addEventListener('keydown', e => {
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(regular[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(sharps[blackKeyIndex])
+    console.log(whiteKeyIndex)
+    console.log(key)
 })
+
+regular.forEach(key => {
+    const K = key.dataset.key
+    // console.log(K)
+    var keyName = document.createElement('H1');
+    const content = document.createTextNode(WHITE_KEYS[K])
+    // keyName.className = "key_btn"
+    keyName.appendChild(content);
+
+    key.appendChild(keyName);
+})
+
+sharps.forEach(key => {
+    const B = key.dataset.key
+    console.log(B)
+    var keyName = document.createElement('H1');
+    const blk_content = document.createTextNode(BLACK_KEYS[B])
+    // keyName.className = "key_btn"
+    console.log(blk_content)
+    keyName.appendChild(blk_content);
+
+    key.appendChild(keyName);
+})
+
+
+
+
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0;
     console.log(key.dataset.note)
     console.log(noteAudio)
     noteAudio.play()
