93d23c6d8adc91a387a21fc0c2aec3eaeb499890
amalagon1 <89949832+amalagon1@users.noreply.github.com>
2023-02-01 00:15:21 -0500

    added basic visuals

--- script.js
+++ script.js
@@ -3,12 +3,61 @@ const regular = document.querySelectorAll('.white-key');
 const sharps = document.querySelectorAll('.black-key');
 const body = document.querySelector('.body');
 const keys = document.querySelectorAll('.key');
+const canvas = document.getElementById('canvas');
+const container = document.querySelector('container');
 
 const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'];
 const BLACK_KEYS = ['w', 'e', 't', 'y', 'u', 'o', 'p'];
 
 
-// const note = new Audio("tones/C-sharp.mp3");
+// set up canvas/audio visualizer
+let audio = new Audio();
+audio.src = "../tones/A.mp3";
+canvas.width = 785;
+canvas.height = 297;
+const ctx = canvas.getContext("2d");
+
+//set up web audio api variables
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let audioSource = null;
+let analyser = null;
+
+// audio.play();
+audioSource = audioCtx.createMediaElementSource(audio);
+analyser = audioCtx.createAnalyser();
+audioSource.connect(analyser);
+analyser.connect(audioCtx.destination);
+
+// calculating visualizer bar dimensions
+
+analyser.fftSize = 128;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+const barWidth = canvas.width / bufferLength;
+
+// animating the bars
+
+let x = 0;
+function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth
+    }
+    requestAnimationFrame(animate);
+}
+
+animate()
+
+document.querySelector(".play").addEventListener("click", () => {
+    audio.play()
+})
+
 
 
 keys.forEach(key => {
@@ -57,8 +106,6 @@ sharps.forEach((key, index) => {
 })
 
 
-
-
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0;
@@ -72,11 +119,11 @@ function playNote(key) {
 }
 
 
-document.querySelector('.button').addEventListener('click', () => {
-    note.play()
-    console.log("clicked")
-    body.classList.add('theme-light')
-})
+// document.querySelector('.button').addEventListener('click', () => {
+//     note.play()
+//     console.log("clicked")
+//     body.classList.add('theme-light')
+// })
 
 
 
